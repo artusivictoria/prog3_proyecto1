b943358f96f1ab89a398b71e251a80cc4b459b51
sophiadeleone <sdeleone@udesa.edu.ar>
2025-04-04 18:12:14 -0930

    ediciones de pelicula y peliculas mas populares a partir del asincronico de integracion con apis y lo d ciclo de vida de un componente

--- src/components/PeliculaMasPop/PeliculaMasPop.js
+++ src/components/PeliculaMasPop/PeliculaMasPop.js
@@ -1,57 +1,103 @@
 import React, { Component } from 'react';
-import "./styles.css"
+import "./styles.css";
 
 class PeliculaMasPop extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: props.data,
-            mostrarContenido: true,
-            textoBoton: 'OCULTAR'
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+      mostrarContenido: true,
+      textoBoton: 'OCULTAR'
+    };
+  }
+
+  ocultar() {
+    if (this.state.mostrarContenido === true) {
+      this.setState({
+        mostrarContenido: false,
+        textoBoton: 'ver mas'
+    });
+} else {
+  this.setState({
+    mostrarContenido: true,
+    textoBoton: 'OCULTAR'
+  });
+}
+}
+
+render() {
+return (
+  <div>
+    <img src={this.state.data.imagen} alt="imagen de peliculaMasPop" />
+    <h4>{this.state.data.nombre}</h4>
+    {this.state.mostrarContenido === true ?       
+      <p>{this.state.data.descripcion}</p>
+      : ''
     }
-    ocultar() {
-        // if(this.state.mostrarContenido === true){
-        //     this.setState({
-        //         mostrarContenido: false,
-        //         textoBoton: 'ver mas'\
-        //     })
-        // } else {
-        //     this.setState({
-        //         mostrarContenido: true,
-        //         textoBoton: 'OCULTAR'
-        //     })
-        // }
-        this.setState({
-            mostrarContenido: !this.state.mostrarContenido,
-            textoBoton: this.state.mostrarContenido === true ? 'ver mas' : 'OCULTAR'
-        })
-        
-
-    }
-    render() {
-        console.log('props de la peliculaMasPop', this.props)
-        return (
-            <div>
-                <img src={this.state.data.imagen} alt="imagen de peliculaMasPop" />
-                <h4>{this.state.data.nombre}</h4>
-                {
-                this.state.mostrarContenido === true ?       
-                <p>{this.state.data.descripcion}</p>
-                : ''
-                }
-                <button onClick={() => this.ocultar()}>
-                   {
-                    this.state.textoBoton
-                   }
-                </button>
-
-            </div>
-
-        );
-    }
-
-
+    <button onClick={() => this.ocultar()}>
+      {this.state.textoBoton}
+    </button>
+  </div>
+);
+}
 }
 
 export default PeliculaMasPop;
+
+// import React, { Component } from 'react';
+// import "./styles.css"
+
+// class PeliculaMasPop extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: props.data,
+//             mostrarContenido: true,
+//             textoBoton: 'OCULTAR'
+//         }
+//     }
+//     ocultar() {
+//         // if(this.state.mostrarContenido === true){
+//         //     this.setState({
+//         //         mostrarContenido: false,
+//         //         textoBoton: 'ver mas'\
+//         //     })
+//         // } else {
+//         //     this.setState({
+//         //         mostrarContenido: true,
+//         //         textoBoton: 'OCULTAR'
+//         //     })
+//         // }
+//         this.setState({
+//             mostrarContenido: !this.state.mostrarContenido,
+//             textoBoton: this.state.mostrarContenido === true ? 'ver mas' : 'OCULTAR'
+//         })
+        
+
+//     }
+//     render() {
+//         console.log('props de la peliculaMasPop', this.props)
+//         return (
+//             <div>
+//                 <img src={this.state.data.imagen} alt="imagen de peliculaMasPop" />
+//                 <h4>{this.state.data.nombre}</h4>
+//                 {
+//                 this.state.mostrarContenido === true ?       
+//                 <p>{this.state.data.descripcion}</p>
+//                 : ''
+//                 }
+//                 <button onClick={() => this.ocultar()}>
+//                    {
+//                     this.state.textoBoton
+//                    }
+//                 </button>
+
+//             </div>
+
+//         );
+//     }
+
+
+// }
+
+// export default PeliculaMasPop;
--- src/components/PeliculasMasPop/PeliculasMasPop.js
+++ src/components/PeliculasMasPop/PeliculasMasPop.js
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import PeliculaMasPop from "../PeliculaMasPop/PeliculaMasPop";
 
-let urlMoviesMasPop= 'https://api.themoviedb.org/3/movie/popular'
+let urlMoviesMasPop = 'https://api.themoviedb.org/3/movie/popular?'; // desp tengo que poner lo de api key!! acordate soph q sino no veo la pag
 
-fetch(urlMoviesMasPop)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data); //Mostrame por consola. //Array de datos que vino de la API
-        function PeliculasMasPop(){
-            let ArrayPelicaulasMasPop = [];
-            for(let i=0; i<data.length; i++){
-                ArrayPelicaulasMasPop += {id: data[i].results.id, imagen: data[i].results.poster_path, nombre: data[i].results.title , descripcion: data[i].results.overview}
-            }
-            return(PeliculasMasPop.map((elm, idx) => <PeliculaMasPop data={elm} /> ))
-        }
-        
+class PeliculasMasPop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peliculas: []
+    };
+  }
 
-    })
-    .catch(function(error){
+  componentDidMount() {
+    fetch(urlMoviesMasPop)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        let peliculasArray = data.results.map(p => ({
+          id: p.id,
+          imagen: p.poster_path,
+          nombre: p.title,
+          descripcion: p.overview
+        }));
+        this.setState({ peliculas: peliculasArray });
+      })
+      .catch((error) => {
         console.log(error);
-    })
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.peliculas.map((peli) => (
+          <PeliculaMasPop key={peli.id} data={peli} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default PeliculasMasPop;
+
+
+
+// import React from "react";
+// import PeliculaMasPop from "../PeliculaMasPop/PeliculaMasPop";
+
+// //me falta poner lo de api key!!!!
+// let urlMoviesMasPop= 'https://api.themoviedb.org/3/movie/popular'
+
+// fetch(urlMoviesMasPop)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data); //Mostrame por consola. //Array de datos que vino de la API
+//         function PeliculasMasPop(){
+//             let ArrayPelicaulasMasPop = [];
+//             for(let i=0; i<data.length; i++){
+//                 ArrayPelicaulasMasPop += {id: data[i].results.id, imagen: data[i].results.poster_path, nombre: data[i].results.title , descripcion: data[i].results.overview}
+//             }
+//             return(PeliculasMasPop.map((elm, idx) => <PeliculaMasPop data={elm} /> ))
+//         }
+        
+
+//     })
+//     .catch(function(error){
+//         console.log(error);
+//     })
+
+// export default PeliculasMasPop;
          

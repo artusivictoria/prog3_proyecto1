e139d7865154cece6e5ed88fa6722a2ad4393f3b
sophiadeleone <sdeleone@udesa.edu.ar>
2025-04-09 21:16:09 -0930

    agregar Favoritos y ediciones

--- src/components/FivePeliculaMasPop/FivePeliculaMasPop.js
+++ src/components/FivePeliculaMasPop/FivePeliculaMasPop.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 //import "./styles.css";
 import '../peliContainer.css'
 
+//aca hay q agregar export default antes de class????
 class FivePeliculaMasPop extends Component {
   constructor(props) {
     super(props);
@@ -19,30 +20,52 @@ class FivePeliculaMasPop extends Component {
       this.setState({
         mostrarContenido: true,
         textoBoton: 'OCULTAR'
-    });
-} else {
-  this.setState({
-    mostrarContenido: false,
-    textoBoton: 'VER MAS'
-  });
-}
-}
-
-render() {
-return (
-  <div className='article'>
-    <img className='imagenP' src={this.state.data.imagen} alt="imagen de peliculaMasPop" />
-    <h4>{this.state.data.nombre}</h4>
-    {this.state.mostrarContenido === true ?       
-      <p>{this.state.data.descripcion}</p>
-      : ''
+      });
+    } else {
+      this.setState({
+        mostrarContenido: false,
+        textoBoton: 'VER MAS'
+      });
     }
-    <button onClick={() => this.ocultar()}>
-      {this.state.textoBoton}
-    </button>
-  </div>
-);
-}
+  }
+
+
+  agregarAFav(id) {
+    let storage = localStorage.getItem('fav')
+    if (storage !== null) {
+      let arrParseado = JSON.parse(storage)
+      arrParseado.push(id)
+      let arrStringificado = JSON.stringify(arrParseado)
+      localStorage.setItem('fav', arrStringificado)
+    } else {
+      let primerID = [id]
+      let arrStringificado = JSON.stringify(primerID)
+      localStorage.setItem('fav', arrStringificado)
+    }
+
+    this.setState({
+      favorito: true
+    })
+  }
+
+
+  render() {
+
+    return (
+
+      <div className='article'>
+        <img className='imagenP' src={this.state.data.imagen} alt="imagen de peliculaMasPop" />
+        <h4>{this.state.data.nombre}</h4>
+        {this.state.mostrarContenido === true ?
+          <p>{this.state.data.descripcion}</p>
+          : ''
+        }
+        <button onClick={() => this.ocultar()}>
+          {this.state.textoBoton}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default FivePeliculaMasPop;
